453e22a07d6f47c2719f9476748d5e34aa35539a
Ervin Lara <ervin.lara@oracle.com>
2020-11-11 18:09:43 -0500

    Added React App to Grocery Store

---
+++ grocery-store-ui/grocery-store-app/src/LogIn/LogInComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LogInComp extends React.Component {
+
+    render(){
+        return(
+            <h1>Hey</h1>
+        );
+    }
+
+}
+
+export default LogInComp
