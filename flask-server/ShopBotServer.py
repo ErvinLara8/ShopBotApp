4eb2e21ecd8fc089cd4a235f94d735a3df95b4a1
Ervin Lara <ervin.lara@oracle.com>
2020-11-30 01:45:46 -0500

    Added Routing through flask app

--- flask-server/ShopBotServer.py
+++ flask-server/ShopBotServer.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+# @app.route('/')
+# def index():
+#     return app.send_static_file('index.html')
+
+@app.errorhandler(404)   
+def not_found(e):   
+  return app.send_static_file('index.html')
+
+
+@app.route("/sample")
+def sample():
+    return "<h1>YOOOOO</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ grocery-store-ui/grocery-store-app/src/OrdersPage/OrdersPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class OrdersPage extends React.Component {
+
+    render(){
+        //const pageTitle = "ShopBot Grocery Store Log In";
+        return(
+            <div>
+                <h1 style={{ textAlign: "center" }}>Youre In orders page</h1>
+            </div>
+        );
+    }
+
+}
+
+export default OrdersPage
