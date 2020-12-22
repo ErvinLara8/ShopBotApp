80033803c0ae7fd9334b398d58158b0a028ca808
Ervin Lara <ervin.lara@oracle.com>
2020-12-21 23:55:39 -0500

    Started NavBar

--- grocery-store-ui/grocery-store-app/src/OrdersPage/OrdersPage.jsx
+++ grocery-store-ui/grocery-store-app/src/OrdersPage/OrdersPage.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class OrdersPage extends React.Component {
 
     render(){
         //const pageTitle = "ShopBot Grocery Store Log In";
         return(
-            <div>
-                <h1 style={{ textAlign: "center" }}>Youre In orders page</h1>
-            </div>
+            <Container>
+                <Navbar expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand>ShopBot</Navbar.Brand>
+                    <Nav fill variant="pills" className="mr-auto">
+                        <Nav.Item>
+                            <Nav.Link href="/OrdersPage">Orders</Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link eventKey="">Products</Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link eventKey="">Employees</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Nav justify variant="pills">
+                        <Navbar.Text>
+                            Signed in as: Blank
+                        </Navbar.Text>
+                        <Nav.Item>
+                            <Nav.Link>Log Out</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar>
+
+            </Container>
         );
     }
 
