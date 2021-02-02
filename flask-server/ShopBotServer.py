2318b6b26dc82d3cf4d4e0ace6a2827cc8d3262d
Imtiaz Vohra <IMTIAZ.VOHRA1@pb.com>
2021-02-02 14:14:01 -0500

    Updates to logic and frontend

--- flask-server/ShopBotServer.py
+++ flask-server/ShopBotServer.py
@@ -12,37 +12,37 @@ def not_found(e):
   return app.send_static_file('index.html')
 
 
-@app.route("/GetOrders", methods=['GET'])
-def sample():
-
-
-  order1 = {"orderID": 1, "userID": 123, "firstName": "Ervin", "lastName": "Lara", "wantedProducts": ["Apples", "Bananas"]}
-  order2 = {"orderID": 2, "userID": 456, "firstName": "Bill", "lastName": "Baker", "wantedProducts": ["Chips", "Cookies"]}
-  completed1 = {"orderID": 3, "userID": 654, "firstName": "Sally", "lastName": "Simpson", "wantedProducts": ["Broccoli", "Cookies"]}
-  completed2 = {"orderID": 4, "userID": 643, "firstName": "Joe", "lastName": "Doe", "wantedProducts": ["Apples", "Oreos"]}
-
-  allPendingOrders = [order1,order2]
-  allCompletedOrders = [completed1,completed2]
-
-
-
-  return jsonify({"pendingOrders": allPendingOrders, "completedOrders":allCompletedOrders })
+# @app.route("/GetOrders", methods=['GET'])
+# def sample():
+#
+#
+#   order1 = {"orderID": 1, "userID": 123, "firstName": "Ervin", "lastName": "Lara", "wantedProducts": ["Apples", "Bananas"]}
+#   order2 = {"orderID": 2, "userID": 456, "firstName": "Bill", "lastName": "Baker", "wantedProducts": ["Chips", "Cookies"]}
+#   completed1 = {"orderID": 3, "userID": 654, "firstName": "Sally", "lastName": "Simpson", "wantedProducts": ["Broccoli", "Cookies"]}
+#   completed2 = {"orderID": 4, "userID": 643, "firstName": "Joe", "lastName": "Doe", "wantedProducts": ["Apples", "Oreos"]}
+#
+#   allPendingOrders = [order1,order2]
+#   allCompletedOrders = [completed1,completed2]
+#
+#
+#
+#   return jsonify({"pendingOrders": allPendingOrders, "completedOrders":allCompletedOrders })
 
 @app.route("/GetEmployees", methods=['GET'])
 def sample():
 
 
-  employee1 = {"employeeID": 1, "firstName": "Saara", "lastName": "Vohra", "password": "myPassword123"}
-  employee2 = {"employeeID": 2, "firstName": "Leya", "lastName": "Michele", "password": "thisIsMyPass00"}
-  newEmployee1 = {"employeeID": 3, "firstName": "Peter", "lastName": "Parker", "password": "Spiderman2021"}
-  newEmployee2 = {"employeeID": 4, "firstName": "Zaid", "lastName": "Beau", "password": "Baseball#11"}
+  employee1 = {"employeeID": 1, "firstName": "Saara", "lastName": "Vohra", "password": "myPassword123", "isAdmin": "true", "isWorking": "true"}
+  employee2 = {"employeeID": 2, "firstName": "Leya", "lastName": "Michele", "password": "thisIsMyPass00", "isAdmin": "false", "isWorking": "true"}
+  newEmployee1 = {"employeeID": 3, "firstName": "Peter", "lastName": "Parker", "password": "Spiderman2021", "isAdmin": "false", "isWorking": "false"}
+  newEmployee2 = {"employeeID": 4, "firstName": "Zaid", "lastName": "Beau", "password": "Baseball#11", "isAdmin": "true", "isWorking": "false"}
 
-  allExistingEmployees = [employee1,employee2]
-  allAddedEmployees = [newEmployee1,newEmployee2]
+  allOnDutyEmployees = [employee1,employee2]
+  allOffDutyEmployees = [newEmployee1,newEmployee2]
 
 
 
-  return jsonify({"existingEmployees":allExistingEmployees, "addedEmployees": allAddedEmployees })
+  return jsonify({"onDutyEmployees":allOnDutyEmployees, "offDutyEmployees": allOffDutyEmployees})
 
 if __name__ == "__main__":
     app.run(debug=True)
